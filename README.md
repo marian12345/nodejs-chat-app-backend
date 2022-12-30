8a5bf40b3f37ec74a3e5f737fec0b18554c86a0f
marian12345 <marian.zwarg@gmx.de>
2022-12-30 13:50:54 +0100

    removed heroku info

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # nodejs-chat-app-backend
-This repository contains the Backend implementation for a chat application developed with Node.js, Socket.io, Express.js, Docker. 
-It is currently deployed on Heroku.
 
-The goal is that users can give themselves a username and chat with eachother. 
+This repository contains the Backend implementation for a chat application developed with Node.js, Socket.io, Express.js, Docker.
+It is currently deployed on fly.io
+
+The goal is that users can give themselves a username and chat with eachother.
 
 Currently there is one Vuejs Frontend existing which can be found [here](https://github.com/marian12345/vue-chat-app-frontend)
-
-
--- server.js
+++ server.js
@@ -1,91 +1,99 @@
 // express und http Module importieren. Sie sind dazu da, die HTML-Dateien
 // aus dem Ordner "public" zu veröffentlichen.
 // add express and http module
-var express = require('express');
-var fs = require('fs');
+var express = require("express");
+var fs = require("fs");
 var app = express();
 
-var server = require('http').createServer(app);
+var server = require("http").createServer(app);
 var port = process.env.PORT || 3000;
 
 // change from http to https server when in production
 /**
  * The following code section is only executed in combination with certbot and
- * when the application is deployed on a public server (production mode). 
- * It is not used when deplyoying on Heroku
+ * when the application is deployed on a public server (production mode).
+ * It is not used when deplyoying on Heroku or fly.io
  */
-if (process.env.NODE_ENV === 'production') {
-    console.log("production mode on");
-    // use https port
-    port = 443;
-    
-    // Certificate for Domain 1
-    const privateKey1 = fs.readFileSync('/etc/letsencrypt/live/www.vivaldiseinhaus.de/privkey.pem', 'utf8');
-    const certificate1 = fs.readFileSync('/etc/letsencrypt/live/www.vivaldiseinhaus.de/cert.pem', 'utf8');
-    const ca1 = fs.readFileSync('/etc/letsencrypt/live/www.vivaldiseinhaus.de/chain.pem', 'utf8');
-    const certCredentials = {
-        key: privateKey1,
-        cert: certificate1,
-        ca: ca1
-    };
+if (process.env.NODE_ENV === "production") {
+  console.log("production mode on");
+  // use https port
+  port = 443;
 
-    server = require('https').createServer(certCredentials, app);
+  // Certificate for Domain 1
+  const privateKey1 = fs.readFileSync(
+    "/etc/letsencrypt/live/www.vivaldiseinhaus.de/privkey.pem",
+    "utf8"
+  );
+  const certificate1 = fs.readFileSync(
+    "/etc/letsencrypt/live/www.vivaldiseinhaus.de/cert.pem",
+    "utf8"
+  );
+  const ca1 = fs.readFileSync(
+    "/etc/letsencrypt/live/www.vivaldiseinhaus.de/chain.pem",
+    "utf8"
+  );
+  const certCredentials = {
+    key: privateKey1,
+    cert: certificate1,
+    ca: ca1,
+  };
+
+  server = require("https").createServer(certCredentials, app);
 }
 
 // add Socket.io
-var io = require('socket.io')(server, {
-    cors: {
-      origin: ["https://marian12345.github.io", "http://localhost:8080"],
-      credentials: true,
-      perMessageDeflate :false
-    }
+var io = require("socket.io")(server, {
+  cors: {
+    origin: ["https://marian12345.github.io", "http://localhost:8080"],
+    credentials: true,
+    perMessageDeflate: false,
+  },
 });
 
 // start http web server
 server.listen(port, function () {
-    // write note that web server is running
-    console.log('Webserver läuft und hört auf Port %d', port);
+  // write note that web server is running
+  console.log("Webserver läuft und hört auf Port %d", port);
 });
 
 // add path to public html files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // === Code for Chat-Server - Socket.io code
- 
-io.on('connection', function (socket) {
-    // socket represents the socket connection to client
-    
-    var addedUser = false;
-    
-    // if user enters his name do...
-    socket.on('add_user', function (username) {
-        // store username in current socket connection
-        socket.username = username;
-        addedUser = true;
-            
-        // send message to client so that he knows he is logged in
-        socket.emit('login');
-            
-        // inform all other clients
-        socket.broadcast.emit('user_joined', socket.username);
-    });
-    
-    // when someone sends a message, inform all other clients
-    socket.on('new_message', function (data) {
-        // send message to other clients
-        socket.broadcast.emit('new_message', {
-            username: socket.username,
-            message: data
-        });
-    });
-    
-    // when someone disconnects inform all other clients
-    // users do not have to click on a button to disconnect, it is enough to close the browser window
-    socket.on('disconnect', function () {
-        if (addedUser) {
-            // inform other clients
-            socket.broadcast.emit('user_left', socket.username);
-        }
-    });
-});
 
+io.on("connection", function (socket) {
+  // socket represents the socket connection to client
+
+  var addedUser = false;
+
+  // if user enters his name do...
+  socket.on("add_user", function (username) {
+    // store username in current socket connection
+    socket.username = username;
+    addedUser = true;
+
+    // send message to client so that he knows he is logged in
+    socket.emit("login");
+
+    // inform all other clients
+    socket.broadcast.emit("user_joined", socket.username);
+  });
+
+  // when someone sends a message, inform all other clients
+  socket.on("new_message", function (data) {
+    // send message to other clients
+    socket.broadcast.emit("new_message", {
+      username: socket.username,
+      message: data,
+    });
+  });
+
+  // when someone disconnects inform all other clients
+  // users do not have to click on a button to disconnect, it is enough to close the browser window
+  socket.on("disconnect", function () {
+    if (addedUser) {
+      // inform other clients
+      socket.broadcast.emit("user_left", socket.username);
+    }
+  });
+});
